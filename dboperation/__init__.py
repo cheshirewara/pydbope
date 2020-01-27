91c95651b78617a357130e91c4c78135dc427e96
cheshire boy <info@cheshire-wara.com>
2020-01-27 10:09:24 +0900

    change set up

--- dboperation/__init__.py
+++ dboperation/__init__.py
@@ -5,6 +5,11 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'class'))
 from DBConfigClass import DBConfig
 from DBQueryClass import DBQuery
 
+
+def Hello():
+    print("Hello dboperation")
+
+
 if __name__ == '__main__':
     print(DBConfig().get_inner())
     print(DBQuery().get_lines())
